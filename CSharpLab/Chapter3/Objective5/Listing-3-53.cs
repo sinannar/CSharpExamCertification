7d1d50fad1a70c5fb02a745d204865b08648a077
sinan nar <sinan.nar@gmail.com>
2018-08-27 22:56:43 +1200

    chptr 3 is finished

---
+++ CSharpLab/Chapter3/Objective5/Listing-3-53.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace CSharpLab.Chapter3.Objective5
+{
+    public static class Listing_3_53
+    {
+        public static void RunMain()
+        {
+            //Console.WriteLine("Press escape key to stop");
+            //using (PerformanceCounter pc = new PerformanceCounter("Memory", "Available bytes"))
+            //{
+            //    string text = "Available memory: ";
+            //    Console.Write(text);
+            //    do
+            //    {
+            //        while (!Console.KeyAvailable)
+            //        {
+            //            Console.Write(pc.RawValue);
+            //            Console.SetCursorPosition(text.Length, Console.CursorTop);
+            //        }
+            //    } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            //}
+        }        
+    }
+}
+
---
+++ CSharpLab/Chapter3/Objective5/Listing-3-54.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace CSharpLab.Chapter3.Objective5
+{
+    public static class Listing_3_54
+    {
+        public static void RunMain()
+        {
+            //if (CreatePerformanecCounters())
+            //{
+            //    Console.WriteLine("Created performance counters");
+            //    Console.WriteLine("Please restart the application");
+            //    Console.ReadKey();
+
+            //    return;
+            //}
+
+            //var totalOperationsCounter = new PerformanceCounter(
+            //    "MyCategory",
+            //    "# operations executed",
+            //    "",
+            //    false);
+            //var operationsPerSecondCounter = new PerformanceCounter(
+            //    "MyCategory",
+            //    "# operations sec",
+            //    "",
+            //    false);
+
+            //totalOperationsCounter.Increment();
+            //operationsPerSecondCounter.Increment();
+        }
+
+        //private static bool CreatePerformanecCounters()
+        //{
+        //    if (!PerformanceCounterCategory.Exists("MyCategory"))
+        //    {
+        //        CounterCreationDataCollection counters = new CounterCreationDataCollection()
+        //        {
+        //            new CounterCreationData(
+        //                "# operations executed",
+        //                "Total number of operations executed",
+        //                PerformanceCounterType.NumberOfItems32
+        //                ),
+        //            new CounterCreationData(
+        //                "# operations / sec",
+        //                "Total operations executed per second",
+        //                PerformanceCounterType.NumberOfItems32
+        //                )
+        //        };
+
+        //        PerformanceCounterCategory.Create("MyCategory", "Sample category for Codeproject", counters);
+
+        //        return true;
+        //    }
+
+        //    return false;
+        //}
+    }
+}
+
